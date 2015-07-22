412d2a1bbe3ed862bef16e1462cb364c01829420
kevgliss <kevgliss@gmail.com>
2015-07-22 10:53:35 -0700

    adding testing conf

---
+++ lemur/tests/conf.py
@@ -0,0 +1,88 @@
+
+# This is just Python which means you can inherit and tweak settings
+
+import os
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+ADMINS = frozenset([''])
+
+THREADS_PER_PAGE = 8
+
+#############
+## General ##
+#############
+
+# These will need to be set to `True` if you are developing locally
+CORS = False
+debug = False
+
+TESTING = True
+
+# this is the secret key used by flask session management
+SECRET_KEY = 'I/dVhOZNSMZMqrFJa5tWli6VQccOGudKerq3eWPMSzQNmHHVhMAQfQ=='
+
+# You should consider storing these separately from your config
+LEMUR_TOKEN_SECRET = 'test'
+LEMUR_ENCRYPTION_KEY = 'jPd2xwxgVGXONqghHNq7/S761sffYSrT3UAgKwgtMxbqa0gmKYCfag=='
+
+# this is a list of domains as regexes that only admins can issue
+LEMUR_RESTRICTED_DOMAINS = []
+
+#################
+## Mail Server ##
+#################
+
+# Lemur currently only supports SES for sending email, this address
+# needs to be verified
+LEMUR_EMAIL = ''
+LEMUR_SECURITY_TEAM_EMAIL = []
+
+#############
+## Logging ##
+#############
+
+LOG_LEVEL = "DEBUG"
+LOG_FILE = "lemur.log"
+
+
+##############
+## Database ##
+##############
+
+# modify this if you are not using a local database
+SQLALCHEMY_DATABASE_URI = 'postgresql://lemur:lemur@localhost:5432/lemur'
+
+
+#########
+## AWS ##
+#########
+
+# Lemur will need STS assume role access to every destination you want to monitor
+#AWS_ACCOUNT_MAPPINGS = {
+#    '1111111111': 'myawsacount'
+#}
+
+## This is useful if you know you only want to monitor one destination
+#AWS_REGIONS = ['us-east-1']
+
+#LEMUR_INSTANCE_PROFILE = 'Lemur'
+
+#############
+## Issuers ##
+#############
+
+# These will be dependent on which 3rd party that Lemur is
+# configured to use.
+
+#CLOUDCA_URL = ''
+#CLOUDCA_PEM_PATH = ''
+#CLOUDCA_BUNDLE = ''
+
+# number of years to issue if not specified
+#CLOUDCA_DEFAULT_VALIDITY = 2
+
+#VERISIGN_URL = ''
+#VERISIGN_PEM_PATH = ''
+#VERISIGN_FIRST_NAME = ''
+#VERISIGN_LAST_NAME = ''
+#VERSIGN_EMAIL = ''
