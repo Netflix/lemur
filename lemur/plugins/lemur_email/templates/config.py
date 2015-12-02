aa18b88a61654257101ce307f3885ea6fd8933d1
kevgliss <kevgliss@gmail.com>
2015-12-01 17:13:43 -0800

    Making the notification email template cleaner

--- lemur/plugins/lemur_email/templates/config.py
+++ lemur/plugins/lemur_email/templates/config.py
@@ -1,5 +1,12 @@
 import os
+import arrow
 from jinja2 import Environment, FileSystemLoader
 
 loader = FileSystemLoader(searchpath=os.path.dirname(os.path.realpath(__file__)))
 env = Environment(loader=loader)
+
+
+def human_time(time):
+    return arrow.get(time).format('dddd, MMMM D, YYYY')
+
+env.filters['time'] = human_time
