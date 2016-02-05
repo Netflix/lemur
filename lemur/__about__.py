e8e7bdf9e0a309318bc5b14ff94106b44fe7f582
kevgliss <kevgliss@gmail.com>
2016-02-05 13:00:59 -0800

    adding changelog

--- lemur/__about__.py
+++ lemur/__about__.py
@@ -9,7 +9,7 @@ __title__ = "lemur"
 __summary__ = ("Certificate management and orchestration service")
 __uri__ = "https://github.com/Netflix/lemur"
 
-__version__ = "0.2.2dev"
+__version__ = "0.2.2"
 
 __author__ = "The Lemur developers"
 __email__ = "security@netflix.com"
