2e7de40ed1f5422cd625558a53097b4bc2097850
kevgliss <kevgliss@gmail.com>
2016-05-10 12:58:30 -0700

    Marsmallowing sources

---
+++ lemur/sources/schemas.py
@@ -0,0 +1,32 @@
+"""
+.. module: lemur.sources.schemas
+    :platform: unix
+    :copyright: (c) 2015 by Netflix Inc., see AUTHORS for more
+    :license: Apache, see LICENSE for more details.
+.. moduleauthor:: Kevin Glisson <kglisson@netflix.com>
+"""
+from marshmallow import fields
+
+from lemur.schemas import PluginSchema
+from lemur.common.schema import LemurInputSchema, LemurOutputSchema
+
+
+class SourceInputSchema(LemurInputSchema):
+    label = fields.String(required=True)
+    options = fields.Dict(load_from='sourceOptions', required=True)
+    description = fields.String()
+    plugin = fields.Nested(PluginSchema)
+    active = fields.Boolean()
+
+
+class SourceOutputSchema(LemurOutputSchema):
+    label = fields.String()
+    options = fields.Dict(dump_to='sourceOptions')
+    description = fields.String()
+    plugin = fields.Nested(PluginSchema)
+    fields.Boolean()
+
+
+source_input_schema = SourceInputSchema()
+sources_output_schema = SourceOutputSchema(many=True)
+source_output_schema = SourceOutputSchema()
