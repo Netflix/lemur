7ac4a3e1d372da815966a00fdbe90833af8c8597
kevgliss <kevgliss@gmail.com>
2016-05-10 11:10:27 -0700

    Re-writing tests

--- lemur/tests/test_notifications.py
+++ lemur/tests/test_notifications.py
@@ -1,117 +1,105 @@
-from lemur.notifications.service import *  # noqa
+import pytest
+
 from lemur.notifications.views import *  # noqa
 
 
-def test_crud(session):
-    notification = create('testnotify', 'email-notification', {}, 'notify1', [])
-    assert notification.id > 0
-
-    notification = update(notification.id, 'testnotify2', {}, 'notify2', True, [])
-    assert notification.label == 'testnotify2'
-
-    assert len(get_all()) == 1
-
-    delete(1)
-    assert len(get_all()) == 0
+from .vectors import VALID_ADMIN_HEADER_TOKEN, VALID_USER_HEADER_TOKEN
 
 
-def test_notification_get(client):
-    assert client.get(api.url_for(Notifications, notification_id=1)).status_code == 401
+def test_notification_input_schema(client, notification):
+    from lemur.notifications.schemas import NotificationInputSchema
+
+    input_data = {
+        'label': 'notification1',
+        'options': {},
+        'description': 'my notification',
+        'active': True,
+        'plugin': {
+            'slug': 'email-notification'
+        }
+    }
+
+    data, errors = NotificationInputSchema().load(input_data)
+
+    assert not errors
 
 
-def test_notification_post(client):
-    assert client.post(api.url_for(Notifications, notification_id=1), data={}).status_code == 405
+@pytest.mark.parametrize("token,status", [
+    (VALID_USER_HEADER_TOKEN, 404),
+    (VALID_ADMIN_HEADER_TOKEN, 404),
+    ('', 401)
+])
+def test_notification_get(client, token, status):
+    assert client.get(api.url_for(Notifications, notification_id=1), headers=token).status_code == status
 
 
-def test_notification_put(client):
-    assert client.put(api.url_for(Notifications, notification_id=1), data={}).status_code == 401
+@pytest.mark.parametrize("token,status", [
+    (VALID_USER_HEADER_TOKEN, 405),
+    (VALID_ADMIN_HEADER_TOKEN, 405),
+    ('', 405)
+])
+def test_notification_post_(client, token, status):
+    assert client.post(api.url_for(Notifications, notification_id=1), data={}, headers=token).status_code == status
 
 
-def test_notification_delete(client):
-    assert client.delete(api.url_for(Notifications, notification_id=1)).status_code == 401
+@pytest.mark.parametrize("token,status", [
+    (VALID_USER_HEADER_TOKEN, 400),
+    (VALID_ADMIN_HEADER_TOKEN, 400),
+    ('', 401)
+])
+def test_notification_put(client, token, status):
+    assert client.put(api.url_for(Notifications, notification_id=1), data={}, headers=token).status_code == status
 
 
-def test_notification_patch(client):
-    assert client.patch(api.url_for(Notifications, notification_id=1), data={}).status_code == 405
+@pytest.mark.parametrize("token,status", [
+    (VALID_USER_HEADER_TOKEN, 200),
+    (VALID_ADMIN_HEADER_TOKEN, 200),
+    ('', 401)
+])
+def test_notification_delete(client, token, status):
+    assert client.delete(api.url_for(Notifications, notification_id=1), headers=token).status_code == status
 
 
-VALID_USER_HEADER_TOKEN = {
-    'Authorization': 'Basic ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE0MzUyMzMzNjksInN1YiI6MSwiZXhwIjoxNTIxNTQ2OTY5fQ.1qCi0Ip7mzKbjNh0tVd3_eJOrae3rNa_9MCVdA4WtQI'}
+@pytest.mark.parametrize("token,status", [
+    (VALID_USER_HEADER_TOKEN, 405),
+    (VALID_ADMIN_HEADER_TOKEN, 405),
+    ('', 405)
+])
+def test_notification_patch(client, token, status):
+    assert client.patch(api.url_for(Notifications, notification_id=1), data={}, headers=token).status_code == status
 
 
-def test_auth_notification_get(client):
-    assert client.get(api.url_for(Notifications, notification_id=1), headers=VALID_USER_HEADER_TOKEN).status_code == 200
+@pytest.mark.parametrize("token,status", [
+    (VALID_USER_HEADER_TOKEN, 400),
+    (VALID_ADMIN_HEADER_TOKEN, 400),
+    ('', 401)
+])
+def test_notification_list_post_(client, token, status):
+    assert client.post(api.url_for(NotificationsList), data={}, headers=token).status_code == status
 
 
-def test_auth_notification_post_(client):
-    assert client.post(api.url_for(Notifications, notification_id=1), data={}, headers=VALID_USER_HEADER_TOKEN).status_code == 405
+@pytest.mark.parametrize("token,status", [
+    (VALID_USER_HEADER_TOKEN, 200),
+    (VALID_ADMIN_HEADER_TOKEN, 200),
+    ('', 401)
+])
+def test_notification_list_get(client, token, status):
+    assert client.get(api.url_for(NotificationsList), headers=token).status_code == status
 
 
-def test_auth_notification_put(client):
-    assert client.put(api.url_for(Notifications, notification_id=1), data={}, headers=VALID_USER_HEADER_TOKEN).status_code == 400
+@pytest.mark.parametrize("token,status", [
+    (VALID_USER_HEADER_TOKEN, 405),
+    (VALID_ADMIN_HEADER_TOKEN, 405),
+    ('', 405)
+])
+def test_notification_list_delete(client, token, status):
+    assert client.delete(api.url_for(NotificationsList), headers=token).status_code == status
 
 
-def test_auth_notification_delete(client):
-    assert client.delete(api.url_for(Notifications, notification_id=1), headers=VALID_USER_HEADER_TOKEN).status_code == 200
-
-
-def test_auth_notification_patch(client):
-    assert client.patch(api.url_for(Notifications, notification_id=1), data={}, headers=VALID_USER_HEADER_TOKEN).status_code == 405
-
-
-VALID_ADMIN_HEADER_TOKEN = {
-    'Authorization': 'Basic ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE0MzUyNTAyMTgsInN1YiI6MiwiZXhwIjoxNTIxNTYzODE4fQ.6mbq4-Ro6K5MmuNiTJBB153RDhlM5LGJBjI7GBKkfqA'}
-
-
-def test_admin_notification_get(client):
-    assert client.get(api.url_for(Notifications, notification_id=1), headers=VALID_ADMIN_HEADER_TOKEN).status_code == 200
-
-
-def test_admin_notification_post(client):
-    assert client.post(api.url_for(Notifications, notification_id=1), data={}, headers=VALID_ADMIN_HEADER_TOKEN).status_code == 405
-
-
-def test_admin_notification_put(client):
-    assert client.put(api.url_for(Notifications, notification_id=1), data={}, headers=VALID_ADMIN_HEADER_TOKEN).status_code == 400
-
-
-def test_admin_notification_delete(client):
-    assert client.delete(api.url_for(Notifications, notification_id=1), headers=VALID_ADMIN_HEADER_TOKEN).status_code == 200
-
-
-def test_admin_notification_patch(client):
-    assert client.patch(api.url_for(Notifications, notification_id=1), data={}, headers=VALID_ADMIN_HEADER_TOKEN).status_code == 405
-
-
-def test_notifications_get(client):
-    assert client.get(api.url_for(NotificationsList)).status_code == 401
-
-
-def test_notifications_post(client):
-    assert client.post(api.url_for(NotificationsList), data={}).status_code == 401
-
-
-def test_notifications_put(client):
-    assert client.put(api.url_for(NotificationsList), data={}).status_code == 405
-
-
-def test_notifications_delete(client):
-    assert client.delete(api.url_for(NotificationsList)).status_code == 405
-
-
-def test_notifications_patch(client):
-    assert client.patch(api.url_for(NotificationsList), data={}).status_code == 405
-
-
-def test_auth_notifications_get(client):
-    assert client.get(api.url_for(NotificationsList), headers=VALID_USER_HEADER_TOKEN).status_code == 200
-
-
-def test_auth_notifications_post(client):
-    assert client.post(api.url_for(NotificationsList), data={}, headers=VALID_USER_HEADER_TOKEN).status_code == 400
-
-
-def test_admin_notifications_get(client):
-    resp = client.get(api.url_for(NotificationsList), headers=VALID_ADMIN_HEADER_TOKEN)
-    assert resp.status_code == 200
-    assert resp.json == {'items': [], 'total': 0}
+@pytest.mark.parametrize("token,status", [
+    (VALID_USER_HEADER_TOKEN, 405),
+    (VALID_ADMIN_HEADER_TOKEN, 405),
+    ('', 405)
+])
+def test_notification_list_patch(client, token, status):
+    assert client.patch(api.url_for(NotificationsList), data={}, headers=token).status_code == status
