a644f4562553f57fd51af11430f1239d7caca130
kevgliss <kevgliss@gmail.com>
2016-07-27 12:41:32 -0700

    Adding some simplified reporting.  (#403)
    * Adding issuance report.
    
    * Fixing whitespace.

--- setup.py
+++ setup.py
@@ -62,7 +62,8 @@ install_requires = [
     'boto==2.38.0',  # we might make this optional
     'boto3==1.3.0',
     'acme==0.1.0',
-    'retrying==1.3.3'
+    'retrying==1.3.3',
+    'tabulate==0.7.5'
 ]
 
 tests_require = [
