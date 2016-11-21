73399033eaa94fecece9c32c47cef0ce5a3acbdb
kevgliss <kevgliss@gmail.com>
2016-11-21 13:58:58 -0800

    Removing flask upping flask-restful.

--- setup.py
+++ setup.py
@@ -34,8 +34,8 @@ with open(os.path.join(ROOT, "lemur", "__about__.py")) as f:
 
 
 install_requires = [
-    'Flask==0.11.1',
-    'Flask-RESTful==0.3.3',
+    'Flask==0.10.1',
+    'Flask-RESTful==0.3.5',
     'Flask-SQLAlchemy==2.1',
     'Flask-Script==2.0.5',
     'Flask-Migrate==2.0.1',
