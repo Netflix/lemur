a6a577fb3512f2bf9ccb2047ba6b168034340ef2
Doppins <contact@doppins.com>
2016-12-12 18:27:09 +0000

    Upgrade dependency Flask-Migrate to ==2.0.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ install_requires = [
     'Flask-RESTful==0.3.5',
     'Flask-SQLAlchemy==2.1',
     'Flask-Script==2.0.5',
-    'Flask-Migrate==2.0.0',
+    'Flask-Migrate==2.0.2',
     'Flask-Bcrypt==0.7.1',
     'Flask-Principal==0.4.0',
     'Flask-Mail==0.9.1',
