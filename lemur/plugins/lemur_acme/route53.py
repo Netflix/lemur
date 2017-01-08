4c87e614a4efc8bb311312819c0268d9f230168d
Kevin Glisson <kevgliss@gmail.com>
2017-01-08 10:39:08 -0800

    Adjusting the way that certificates are requested.

--- lemur/plugins/lemur_acme/route53.py
+++ lemur/plugins/lemur_acme/route53.py
@@ -54,33 +54,24 @@ def change_txt_record(action, zone_id, domain, value, client=None):
     return response["ChangeInfo"]["Id"]
 
 
-def create_txt_record(host, value):
-    zone_id = find_zone_id(host)
+def create_txt_record(account_number, host, value):
+    zone_id = find_zone_id(host, account_number=account_number)
     change_id = change_txt_record(
         "CREATE",
         zone_id,
         host,
         value,
+        account_number=account_number
     )
     return zone_id, change_id
 
 
-def delete_txt_record(change_id, host, value):
+def delete_txt_record(change_id, account_number, host, value):
     zone_id, _ = change_id
     change_txt_record(
         "DELETE",
         zone_id,
         host,
-        value
+        value,
+        account_number=account_number
     )
-
-
-@sts_client('route53')
-def wait_for_change(change_id, client=None):
-    _, change_id = change_id
-
-    while True:
-        response = client.get_change(Id=change_id)
-        if response["ChangeInfo"]["Status"] == "INSYNC":
-            return
-        time.sleep(5)
