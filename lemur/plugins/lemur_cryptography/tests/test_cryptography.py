3a988b8ce4bb277d015a334800655d2e486bb6df
Neil Schelly <nschelly@dyn.com>
2017-01-20 16:30:52 -0500

    create_csr should be good now with these options, and chain certs will be blank in tests

--- lemur/plugins/lemur_cryptography/tests/test_cryptography.py
+++ lemur/plugins/lemur_cryptography/tests/test_cryptography.py
@@ -15,13 +15,14 @@ def test_build_certificate_authority():
         'validity_start': arrow.get('2016-12-01').datetime,
         'validity_end': arrow.get('2016-12-02').datetime,
         'first_serial': 1,
+        'serial_number': 1,
         'owner': 'owner@example.com'
     }
     cert_pem, private_key_pem, chain_cert_pem = build_certificate_authority(options)
 
     assert cert_pem
     assert private_key_pem
-    assert chain_cert_pem
+    assert chain_cert_pem == ''
 
 
 def test_issue_certificate(authority):
@@ -35,4 +36,4 @@ def test_issue_certificate(authority):
     }
     cert_pem, chain_cert_pem = issue_certificate(CSR_STR, options)
     assert cert_pem
-    assert chain_cert_pem
+    assert chain_cert_pem == ''
