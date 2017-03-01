3ac5f3602577b346f2595e378e165d1251a9bb38
Doppins <contact@doppins.com>
2017-03-01 14:54:49 +0000

    Upgrade dependency psycopg2 to ==2.7

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ install_requires = [
     'Flask-Mail==0.9.1',
     'SQLAlchemy-Utils==0.32.12',
     'requests==2.11.1',
-    'psycopg2==2.6.2',
+    'psycopg2==2.7',
     'arrow==0.10.0',
     'six==1.10.0',
     'gunicorn==19.6.0',
