6abb59efc799b892e1113a8a9664fbca9986923d
Doppins <contact@doppins.com>
2017-03-13 11:38:32 +0000

    Upgrade dependency psycopg2 to ==2.7.1

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ install_requires = [
     'SQLAlchemy-Utils==0.32.12',
     'requests==2.11.1',
     'ndg-httpsclient==0.4.2',
-    'psycopg2==2.6.2',
+    'psycopg2==2.7.1',
     'arrow==0.10.0',
     'six==1.10.0',
     'gunicorn==19.7.0',
