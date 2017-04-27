94e717219080e2ba7b065571f3886a433301cf1f
Rick Breidenstein <RickB17@users.noreply.github.com>
2017-04-26 21:19:01 -0400

    Update remote_host.py

--- lemur/plugins/lemur_linuxdst/remote_host.py
+++ lemur/plugins/lemur_linuxdst/remote_host.py
@@ -8,12 +8,19 @@ def copy_cert(cert_cn, dst_user, dst_priv, dst_priv_key, dst_host, dst_port, dst
 
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    #include the private key password if required
     if dst_priv_key is None:
         priv_key = paramiko.RSAKey.from_private_key_file(dst_priv)
     else:
         priv_key = paramiko.RSAKey.from_private_key_file(dst_priv, dst_priv_key)
+    #open the sftp connection
     ssh.connect(dst_host, username=dst_user, port=dst_port, pkey=priv_key)
     sftp = ssh.open_sftp()
+    #make the directory on the desitination server
+    #files will be in a a folder based on the cert_cn
+    #example:
+    #destination folder: /etc/nginx/certs/
+    #files will go in: /etc/nginx/certs/your.cn.com/cert.pem
     try:
         sftp.mkdir(dst_dir)
     except IOError:
@@ -36,12 +43,14 @@ def create_cert(name, dst_dir, export_type, dst_user, dst_priv, dst_priv_key, ds
     dst_file = 'cert.pem'
     chain_req = False
     if export_type == 'NGINX':
+        #This process will result in a cert.pem file with the body and chain in a single file
         if lem_cert.chain is None:
             dst_data = lem_cert.body
         else:
             dst_data = lem_cert.body + '\n' + lem_cert.chain
         chain_req = False
     elif export_type == '3File':
+        #This process will results in three files. cert.pem, priv.key, chain.pem
         dst_data = lem_cert.body
         chain_req = True
     else:
