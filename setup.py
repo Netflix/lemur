799a6cf508ee44bb6adf72d21fd21d2b25e01325
Doppins <contact@doppins.com>
2017-05-27 02:24:36 +0000

    Upgrade dependency requests to ==2.15.1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ install_requires = [
     'Flask-Principal==0.4.0',
     'Flask-Mail==0.9.1',
     'SQLAlchemy-Utils==0.32.14',
-    'requests==2.15.0',
+    'requests==2.15.1',
     'ndg-httpsclient==0.4.2',
     'psycopg2==2.7.1',
     'arrow==0.10.0',
