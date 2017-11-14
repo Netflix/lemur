4b544ae2073c481464797301e9db8028b19ce448
James Chuong <chuong.james@gmail.com>
2017-11-14 10:11:06 -0800

    CSR Export Plugin (#988)
    This plugin allows a certificate to be exported as a CSR via OpenSSL
    x509.  The workflow will be:
    * Create self-signed cert via Cryptography authority
    * Export CSR via this plugin
    * Sign your own cert outside of Lemur
    * Import new cert with private key
    
    Change-Id: Id3f7db2506bd959236cd3a6df622841058abda5a

---
+++ lemur/plugins/lemur_csr/tests/test_csr_export.py
@@ -0,0 +1,13 @@
+import pytest
+from lemur.tests.vectors import INTERNAL_PRIVATE_KEY_A_STR, INTERNAL_CERTIFICATE_A_STR
+
+
+def test_export_certificate_to_csr(app):
+    from lemur.plugins.base import plugins
+    p = plugins.get('openssl-csr')
+    options = []
+    with pytest.raises(Exception):
+        p.export(INTERNAL_CERTIFICATE_A_STR, "", "", options)
+
+    raw = p.export(INTERNAL_CERTIFICATE_A_STR, "", INTERNAL_PRIVATE_KEY_A_STR, options)
+    assert raw != b""
