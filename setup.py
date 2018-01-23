b9764a7d4e4aa4390c00a791b298f3659f3ada30
Doppins <contact@doppins.com>
2018-01-23 03:32:25 +0000

    Upgrade dependency boto3 to ==1.5.20

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ install_requires = [
     'lockfile==0.12.2',
     'inflection==0.3.1',
     'future==0.16.0',
-    'boto3==1.5.19',
+    'boto3==1.5.20',
     'acme==0.20.0',
     'retrying==1.3.3',
     'tabulate==0.8.2',
