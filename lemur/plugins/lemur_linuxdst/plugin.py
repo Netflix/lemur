c20b9c37f1f16890cc06424edb62e5b579dc5977
Dmitry Zykov <i@dmizyk.com>
2018-03-22 17:18:18 +0200

    add validation for port field in linuxdst plugin

--- lemur/plugins/lemur_linuxdst/plugin.py
+++ lemur/plugins/lemur_linuxdst/plugin.py
@@ -24,6 +24,7 @@ class LinuxDstPlugin(DestinationPlugin):
             'type': 'int',
             'required': True,
             'helpMessage': 'This is the port SSHD is running on',
+            'validation': '^(6553[0-5]|655[0-2][0-9]\d|65[0-4](\d){2}|6[0-4](\d){3}|[1-5](\d){4}|[1-9](\d){0,3})',
             'default': '22'
         },
         {
