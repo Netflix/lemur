cced9a7980fe1eaa8f08368678063341e6ced0a5
Dmitry Zykov <i@dmizyk.com>
2018-04-03 13:28:10 +0300

    add sftp destination plugin

--- setup.py
+++ setup.py
@@ -147,7 +147,8 @@ setup(
             'digicert_issuer = lemur.plugins.lemur_digicert.plugin:DigiCertIssuerPlugin',
             'digicert_cis_issuer = lemur.plugins.lemur_digicert.plugin:DigiCertCISIssuerPlugin',
             'digicert_cis_source = lemur.plugins.lemur_digicert.plugin:DigiCertCISSourcePlugin',
-            'csr_export = lemur.plugins.lemur_csr.plugin:CSRExportPlugin'
+            'csr_export = lemur.plugins.lemur_csr.plugin:CSRExportPlugin',
+            'sftp_destination = lemur.plugins.lemur_sftp.plugin:SFTPDestinationPlugin'
         ],
     },
     classifiers=[
