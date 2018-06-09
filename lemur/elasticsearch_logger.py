a16c8aaa9172a9c8c0f3b2319fceeb1aa9478755
Curtis Castrapel <ccastrapel@netflix.com>
2018-06-09 10:01:53 -0700

    Elasticsearch logging configuration

---
+++ lemur/elasticsearch_logger.py
@@ -0,0 +1,21 @@
+import datetime
+
+from elasticsearch import Elasticsearch
+from logging import getLogger, Handler, ERROR
+from pytz import timezone
+
+class ESHandler(Handler):
+    def __init__(self, es, index):
+        super().__init__()
+        self.es = Elasticsearch(es)
+        self.index = index
+        getLogger("elasticsearch").setLevel(ERROR)
+        getLogger("elasticsearch.trace").setLevel(ERROR)
+        getLogger("urllib3").setLevel(ERROR)
+
+    def emit(self, record):
+        record.eventTime = datetime.datetime.now(timezone('US/Pacific')).isoformat()
+        log_entry = self.format(record)
+
+        # Push action to ES
+        return self.es.index(index=self.index, doc_type="python_log", body=log_entry)
