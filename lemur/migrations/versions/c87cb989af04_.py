e91d8ec81b5ecb1569ad0a2aa32ee5dd7fbcce82
Curtis Castrapel <ccastrapel@gmail.com>
2018-10-11 11:36:50 -0700

    add indexes to domains and certificates tables to optimize load time

---
+++ lemur/migrations/versions/c87cb989af04_.py
@@ -0,0 +1,19 @@
+"""Create an index on the domains table for the domain name
+Revision ID: c87cb989af04
+Revises: 9392b9f9a805
+Create Date: 2018-10-11 09:44:57.099854
+
+"""
+
+revision = 'c87cb989af04'
+down_revision = '9392b9f9a805'
+
+from alembic import op
+
+
+def upgrade():
+    op.create_index(op.f('ix_domains_name'), 'domains', ['name'], unique=False)
+
+
+def downgrade():
+    op.drop_index(op.f('ix_domains_name'), table_name='domains')
