efea70b7afb19a32b48050b6b45cc8e9dfac4e2d
Hossein Shafagh <hshafagh@netflix.com>
2019-02-25 19:05:54 -0800

    the check_cert_signature() method was attempting to compare RSA and ECC signatures. If a ec public-key certificate is signed with an RSA key, then it can't be a self-signed certificate, in which case we just raise InvalidSignature.

--- lemur/common/utils.py
+++ lemur/common/utils.py
@@ -201,6 +201,8 @@ def is_selfsigned(cert):
         return True
     except InvalidSignature:
         return False
+    except UnsupportedAlgorithm as e:
+        raise Exception(e)
 
 
 def is_weekend(date):
