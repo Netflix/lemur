61415d3148ae1abcaa77cf5a4c18c80d3dd28062
Hossein Shafagh <hshafagh@netflix.com>
2019-02-26 16:36:59 -0800

    updating cryptography API call, to create right signing algorithm object.

--- lemur/common/utils.py
+++ lemur/common/utils.py
@@ -201,8 +201,6 @@ def is_selfsigned(cert):
         return True
     except InvalidSignature:
         return False
-    except UnsupportedAlgorithm as e:
-        raise Exception(e)
 
 
 def is_weekend(date):
