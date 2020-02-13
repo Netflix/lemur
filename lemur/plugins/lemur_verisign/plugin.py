2b849a65205a00ea129247263c4ac300f8ebd25d
Hossein Shafagh <hoss.sha@gmail.com>
2020-02-13 15:58:07 -0800

    Update plugin.py
    making lint happy

--- lemur/plugins/lemur_verisign/plugin.py
+++ lemur/plugins/lemur_verisign/plugin.py
@@ -110,7 +110,6 @@ def process_options(options):
     }
 
     data["subject_alt_names"] = ",".join(get_additional_names(options))
-
     
     if options.get("validity_end"):
         # VeriSign (Symantec) only accepts strictly smaller than 2 year end date
