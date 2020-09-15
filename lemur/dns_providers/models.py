676562ffdea03035a4c35d1af41a01746f2f304c
sayali <scharhate@netflix.com>
2020-09-14 18:13:35 -0700

    Match column type to db schema No functional change

--- lemur/dns_providers/models.py
+++ lemur/dns_providers/models.py
@@ -12,7 +12,7 @@ class DnsProvider(db.Model):
     __tablename__ = "dns_providers"
     id = Column(Integer(), primary_key=True)
     name = Column(String(length=256), unique=True, nullable=True)
-    description = Column(Text(), nullable=True)
+    description = Column(String(length=1024), nullable=True)
     provider_type = Column(String(length=256), nullable=True)
     credentials = Column(Vault, nullable=True)
     api_endpoint = Column(String(length=256), nullable=True)
