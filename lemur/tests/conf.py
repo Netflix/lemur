21e9a4508df4323bec5feed0a2d3193633fc5b45
Hossein Shafagh <hshafagh@netflix.com>
2020-09-18 17:42:28 -0700

    TypeError: 'float' object cannot be interpreted as an integer

--- lemur/tests/conf.py
+++ lemur/tests/conf.py
@@ -10,10 +10,10 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 
 # generate random secrets for unittest
 def get_random_secret(length):
-    secret_key = ''.join(random.choice(string.ascii_uppercase) for x in range(length/4))
-    secret_key = secret_key + ''.join(random.choice("~!@#$%^&*()_+") for x in range(length/4))
-    secret_key = secret_key + ''.join(random.choice(string.ascii_lowercase) for x in range(length/4))
-    return secret_key + ''.join(random.choice(string.digits) for x in range(length/4))
+    secret_key = ''.join(random.choice(string.ascii_uppercase) for x in range(round(length/4)))
+    secret_key = secret_key + ''.join(random.choice("~!@#$%^&*()_+") for x in range(round(length/4)))
+    secret_key = secret_key + ''.join(random.choice(string.ascii_lowercase) for x in range(round(length/4)))
+    return secret_key + ''.join(random.choice(string.digits) for x in range(round(length/4)))
 
 
 THREADS_PER_PAGE = 8
