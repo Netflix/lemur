19a678dcc258e1619a1a4e806ce66146cb4f7dc7
Hossein Shafagh <hshafagh@netflix.com>
2020-09-19 08:58:52 -0700

    removing typo

--- docker/src/lemur.conf.py
+++ docker/src/lemur.conf.py
@@ -14,7 +14,7 @@ def get_random_secret(length):
     secret_key = ''.join(random.choice(string.ascii_uppercase) for x in range(round(length / 4)))
     secret_key = secret_key + ''.join(random.choice("~!@#$%^&*()_+") for x in range(round(length / 4)))
     secret_key = secret_key + ''.join(random.choice(string.ascii_lowercase) for x in range(round(length / 4)))
-    return secret_key + ''.join(random.choice(string.digits) for x in range(round(length / 4))) soi
+    return secret_key + ''.join(random.choice(string.digits) for x in range(round(length / 4)))
 
 
 SECRET_KEY = repr(os.environ.get('SECRET_KEY', get_random_secret(32).encode('utf8')))
