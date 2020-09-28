96eada297f9d01792aa3640e3e5e456380eee1a2
Hossein Shafagh <hoss.sha@gmail.com>
2020-09-28 14:40:56 -0700

    lint

--- lemur/tests/conf.py
+++ lemur/tests/conf.py
@@ -15,6 +15,7 @@ def get_random_secret(length):
     secret_key = secret_key + ''.join(random.choice(string.ascii_lowercase) for x in range(round(length / 4)))
     return secret_key + ''.join(random.choice(string.digits) for x in range(round(length / 4)))
 
+
 THREADS_PER_PAGE = 8
 
 # General
