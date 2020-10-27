e9824a6808f4007f76df26427a4a10d76ab1b031
Emmanuel Garette <egarette@cadoles.com>
2020-10-27 20:38:00 +0100

    change the log level to info if upgrade is successful

--- lemur/migrations/versions/c301c59688d2_.py
+++ lemur/migrations/versions/c301c59688d2_.py
@@ -110,7 +110,7 @@ def update_key_type():
         except ValueError as e:
             log.error("Error in processing certificate - ID: %s Error: %s \n" % (cert_id, str(e)))
         else:
-            log.error("Processing certificate - ID: %s key_type: %s\n" % (cert_id, cert_key_type))
+            log.info("Processing certificate - ID: %s key_type: %s\n" % (cert_id, cert_key_type))
             stmt = text(
                 "update certificates set key_type=:key_type where id=:id"
             )
