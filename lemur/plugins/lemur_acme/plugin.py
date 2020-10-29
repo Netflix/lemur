33a006bbeba013e4e3f99c5f257458d9199c942a
csine-nflx <csine@netflix.com>
2020-10-28 22:24:37 -0700

    fixing delete with optional validation

--- lemur/plugins/lemur_acme/plugin.py
+++ lemur/plugins/lemur_acme/plugin.py
@@ -385,11 +385,9 @@ class AcmeHandler(object):
                     dns_provider_options = json.loads(dns_provider.credentials)
                     account_number = dns_provider_options.get("account_id")
                     host_to_validate, _ = self.strip_wildcard(authz_record.host)
-                    host_to_validate = self.maybe_add_extension(
-                        host_to_validate, dns_provider_options
-                    )
+                    host_to_validate = self.maybe_add_extension(host_to_validate, dns_provider_options)
                     if authz_record.domain == authz_record.host:
-                        host_to_validate = dns_challenge.validation_domain_name(host_to_validate),
+                        host_to_validate = challenges.DNS01().validation_domain_name(host_to_validate)
                     dns_provider_plugin.delete_txt_record(
                         authz_record.change_id,
                         account_number,
