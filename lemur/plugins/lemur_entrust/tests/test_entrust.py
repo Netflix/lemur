ff540069e29138434348408f1f418a030aebbb09
sirferl <ferdinand.glatzl@bawagpsk.com>
2020-11-13 08:08:54 +0100

    Entrust: add organization handling-Update test

--- lemur/plugins/lemur_entrust/tests/test_entrust.py
+++ lemur/plugins/lemur_entrust/tests/test_entrust.py
@@ -56,7 +56,9 @@ def test_process_options(mock_current_app, authority):
             "requesterName": mock_current_app.config.get("ENTRUST_NAME"),
             "requesterEmail": mock_current_app.config.get("ENTRUST_EMAIL"),
             "requesterPhone": mock_current_app.config.get("ENTRUST_PHONE")
-        }
+        },
+        "org":  "Example, Inc.",
+        "clientID": 1
     }
 
     client_id = 1
