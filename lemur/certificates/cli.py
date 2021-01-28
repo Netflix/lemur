0d7e8d77e48bb48d76c28b869876a8073b84f348
Hossein Shafagh <hshafagh@netflix.com>
2021-01-28 15:49:00 -0800

    comments

--- lemur/certificates/cli.py
+++ lemur/certificates/cli.py
@@ -249,6 +249,8 @@ def rotate(endpoint_name, new_certificate_name, old_certificate_name, message, c
                 current_app.logger.info(log_data)
 
         else:
+            # no certificate name or endpoint was provided, so we will now fetch all endpoints,
+            # which have are attached to a certificate that has been replaced
             print("[+] Rotating all endpoints that have new certificates available")
             for endpoint in endpoint_service.get_all_pending_rotation():
                 log_data["endpoint"] = endpoint.dnsname
