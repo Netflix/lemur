002e73e1844b33b05eef274335f2c92e34a2d38c
sayali <scharhate@netflix.com>
2021-02-01 15:46:22 -0800

    Indentation

--- gulp/karma.conf.js
+++ gulp/karma.conf.js
@@ -2,36 +2,36 @@
 'use strict';
 
 module.exports = function (config) {
-    config.set({
-        basePath: '../',
+  config.set({
+    basePath: '../',
 
-        // Fix for "JASMINE is not supported anymore" warning
-        frameworks: ['jasmine'],
+    // Fix for "JASMINE is not supported anymore" warning
+    frameworks: ['jasmine'],
 
-        files: [
-            'app/lib/angular/angular.js',
-            'app/lib/angular/angular-*.js',
-            'test/lib/angular/angular-mocks.js',
-            'app/js/**/*.js',
-            'test/unit/**/*.js'
-        ],
+    files: [
+      'app/lib/angular/angular.js',
+      'app/lib/angular/angular-*.js',
+      'test/lib/angular/angular-mocks.js',
+      'app/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
 
-        autoWatch: true,
+    autoWatch: true,
 
-        browsers: [process.env.TRAVIS ? 'Chrome_travis_ci' : 'Chrome'],
-        customLaunchers: {
-            'Chrome_travis_ci': {
-                base: 'Chrome',
-                flags: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu',],
-            },
-        },
+    browsers: [process.env.TRAVIS ? 'Chrome_travis_ci' : 'Chrome'],
+    customLaunchers: {
+      'Chrome_travis_ci': {
+        base: 'Chrome',
+        flags: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu',],
+      },
+    },
 
-        junitReporter: {
-            outputFile: 'test_out/unit.xml',
-            suite: 'unit'
-            //...
-        },
+    junitReporter: {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+      //...
+    },
 
-        failOnEmptyTestSuite: false,
-    });
+    failOnEmptyTestSuite: false,
+  });
 };
