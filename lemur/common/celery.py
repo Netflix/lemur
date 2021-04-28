c2419c73602f093e244db0636d62f9b6524f4297
Hossein Shafagh <hshafagh@netflix.com>
2021-04-28 14:29:36 -0700

    language

--- lemur/common/celery.py
+++ lemur/common/celery.py
@@ -960,12 +960,12 @@ def deactivate_entrust_test_certificates():
 def disable_rotation_of_duplicate_certificates():
     """
     We occasionally get duplicate certificates with Let's encrypt. Figure out the set of duplicate certificates and
-    disable auto-rotate if no endpoint is attached to the certificate. This way only the certificated with endpoints
+    disable auto-rotate if no endpoint is attached to the certificate. This way only the certificate with endpoints
     will get auto-rotated. If none of the certs have endpoint attached, to be on the safe side, keep auto rotate ON
-    for one of the certs. This tasks considers all the certificates issued by authority listed in the config
+    for one of the certs. This task considers all the certificates issued by the authorities listed in the config
     AUTHORITY_TO_DISABLE_ROTATE_OF_DUPLICATE_CERTIFICATES
     Determining duplicate certificates: certificates sharing same name prefix (followed by serial number to make it
-    unique) that have same validity, owner, destination, SAN.
+    unique) which have same validity length (issuance and expiration), owner, destination, SANs.
     :return:
     """
 
