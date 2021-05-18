dcc2581760418adce9187234eb03ba5ef664c442
Jasmine Schladen <jschladen@netflix.com>
2021-05-17 18:45:16 -0700

    Include security team in recipients

--- lemur/notifications/messaging.py
+++ lemur/notifications/messaging.py
@@ -377,13 +377,15 @@ def send_rotation_notification(certificate):
 def send_reissue_no_endpoints_notification(new_cert):
     data = certificate_notification_output_schema.dump(new_cert).data
     data["security_email"] = current_app.config.get("LEMUR_SECURITY_TEAM_EMAIL")
-    return send_default_notification("reissued_with_no_endpoints", data, [data["owner"]])
+    email_recipients = [data["owner"]] + data["security_email"]
+    return send_default_notification("reissued_with_no_endpoints", data, email_recipients)
 
 
 def send_reissue_failed_notification(certificate):
     data = certificate_notification_output_schema.dump(certificate).data
     data["security_email"] = current_app.config.get("LEMUR_SECURITY_TEAM_EMAIL")
-    return send_default_notification("reissue_failed", data, [data["owner"]])
+    email_recipients = [data["owner"]] + data["security_email"]
+    return send_default_notification("reissue_failed", data, email_recipients)
 
 
 def send_pending_failure_notification(
