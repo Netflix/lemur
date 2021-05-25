ee32647b77a050b942e78f1d8393a31cf677711f
Sam Havron <shavron@squarespace.com>
2021-05-25 11:44:44 -0400

    put runtime oauth secret in flask session

--- lemur/tests/test_oauth.py
+++ lemur/tests/test_oauth.py
@@ -1,8 +1,10 @@
 from datetime import datetime, timedelta
 from freezegun import freeze_time
 
+from flask import current_app
+
 from lemur.auth.views import *  # noqa
-from lemur.tests.conf import OAUTH_STATE_TOKEN_STALE_TOLERANCE_SECONDS
+from lemur.tests.conf import OAUTH_STATE_TOKEN_STALE_TOLERANCE_SECONDS, OAUTH_STATE_TOKEN_SECRET
 
 
 def test_build_hmac(client):
@@ -10,6 +12,12 @@ def test_build_hmac(client):
 
     assert isinstance(build_hmac(), hmac.HMAC)
 
+    # make a bad key
+    current_app.config["OAUTH_STATE_TOKEN_SECRET"] = 'not-bytes-like'
+    assert not build_hmac()
+
+    # put back a good key, for remaining tests
+    current_app.config["OAUTH_STATE_TOKEN_SECRET"] = OAUTH_STATE_TOKEN_SECRET
 
 def test_generate_state_token(client):
     from lemur.auth.views import generate_state_token
@@ -32,3 +40,8 @@ def test_verify_state_token(client):
     assert not verify_state_token('123456::f4k8')
     assert not verify_state_token('123456f4k8')
     assert not verify_state_token('')
+
+    # force a new key to get generated and stored at runtime
+    current_app.config["OAUTH_STATE_TOKEN_SECRET"] = None
+    token_via_runtime_key = generate_state_token()
+    assert verify_state_token(token_via_runtime_key)
