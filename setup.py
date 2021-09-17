9fd0394b832548fe319ec28a87f1b3b0a04fbc25
Anurag Kumar <mailanu98@gmail.com>
2021-09-17 17:45:28 +0530

    Update setup.py
    updated classifiers

--- setup.py
+++ setup.py
@@ -168,6 +168,10 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Software Development',
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Natural Language :: English",
         "License :: OSI Approved :: Apache Software License"
     ]
