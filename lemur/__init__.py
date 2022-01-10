0fd704346208fe832fe3b2baf92d686397177d79
Joe McGinnis <joe.mcginnis@infor.com>
2022-01-10 18:30:15 -0500

    update response headers for cors

--- lemur/__init__.py
+++ lemur/__init__.py
@@ -125,4 +125,11 @@ def configure_hook(app):
         # Record our response time metric
         metrics.send("response_time", "TIMER", elapsed, metric_tags=tags)
         metrics.send("status_code_{}".format(response.status_code), "counter", 1)
+
+        # If CORS, update response headers
+        if app.config["CORS"]:
+            response.headers.add("Access-Control-Allow-Credentials", "true")
+            response.headers.add("Access-Control-Allow-Headers", app.config.get("CORS_HEADERS", "*"))
+            response.headers.add("Access-Control-Allow-Origin", app.config.get("CORS_ORIGIN", "*"))
+
         return response
