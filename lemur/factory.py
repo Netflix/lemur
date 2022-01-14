2b0e4e7f9b0c23cdba17c1aa22a8e63d49f6f047
Joe McGinnis <joe.mcginnis@infor.com>
2022-01-13 23:41:30 -0500

    add expose headers config var

--- lemur/factory.py
+++ lemur/factory.py
@@ -161,13 +161,15 @@ def configure_extensions(app):
         )
 
     if app.config.get("CORS"):
-        allow_headers = app.config.get("CORS_ALLOW_HEADERS", "content-type")
         origins = app.config.get("CORS_ORIGIN", "*")
+        allow_headers = app.config.get("CORS_ALLOW_HEADERS", "content-type")
+        expose_headers = app.config.get("CORS_EXPOSE_HEADERS", "content-type")
         cors.init_app(
             app,
             resources=r"/api/*",
             origins=origins if origins == "*" else [origin.strip() for origin in origins.split(',')],
             allow_headers=[header.strip() for header in allow_headers.split(',')],
+            expose_headers=[header.strip() for header in expose_headers.split(',')],
             methods=app.config.get("CORS_ALLOW_METHODS", "GET,PUT,POST,DELETE,OPTIONS"),
             supports_credentials=True
         )
