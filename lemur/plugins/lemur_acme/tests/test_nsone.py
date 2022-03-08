6c1f6b8719b0dbf75804fb1c6a5c7f0bff9afbeb
Christopher <chris@alwaysjolley.com>
2022-03-08 09:29:36 -0500

    testing cleanup

--- lemur/plugins/lemur_acme/tests/test_nsone.py
+++ lemur/plugins/lemur_acme/tests/test_nsone.py
@@ -1,3 +1,6 @@
+"""
+Unit Tests for NSone DNS provider
+"""
 import unittest
 from unittest.mock import patch, Mock
 
@@ -6,8 +9,14 @@ from lemur.plugins.lemur_acme import plugin, nsone
 
 
 class TestNsone(unittest.TestCase):
+    """
+    Class for testing NSone plugin
+    """
     @patch("lemur.plugins.lemur_acme.plugin.dns_provider_service")
     def setUp(self, mock_dns_provider_service):
+        """
+        unittest setup
+        """
         self.ACMEIssuerPlugin = plugin.ACMEIssuerPlugin()
         self.acme = plugin.AcmeHandler()
         mock_dns_provider = Mock()
@@ -19,8 +28,9 @@ class TestNsone(unittest.TestCase):
             "test.fakedomain.net": [mock_dns_provider],
         }
 
-        # Creates a new Flask application for a test duration. In python 3.8, manual push of application context is
-        # needed to run tests in dev environment without getting error 'Working outside of application context'.
+        # Creates a new Flask application for a test duration. In python 3.8,
+        # manual push of application context is needed to run tests in dev environment
+        # without getting error 'Working outside of application context'.
         _app = Flask('lemur_test_acme')
         self.ctx = _app.app_context()
         assert self.ctx
@@ -31,6 +41,9 @@ class TestNsone(unittest.TestCase):
 
     @patch("lemur.plugins.lemur_acme.nsone.current_app")
     def test_get_zones(self, mock_current_app):
+        """
+        Testing get_zones method
+        """
         account_number = "1234567890"
         path = "a/b/c"
         zones = ['example.com', 'test.example.com']
@@ -52,7 +65,6 @@ class TestNsone(unittest.TestCase):
                 "laditude":0,
                 "low_watermark":0,
                 "note":"string",
-                "priority":"string",
                 "priority":0,
                 "pulsar":"string",
                 "requests":0,
@@ -65,7 +77,13 @@ class TestNsone(unittest.TestCase):
             "primary":{
                 "enabled":True,
                 "secondaries":[{"ip":"string","networks":[0],"notify":True,"port":0}]
-            },"records":[{"domain":"string","id":"string","short_answers":["string"],"tier":0,"ttl":0,"type":"string"}],
+            },"records":[{
+                "domain":"string",
+                "id":"string",
+                "short_answers":["string"],
+                "tier":0,
+                "ttl":0,
+                "type":"string"}],
             "refresh":0,
             "retry":0,
             "ttl":0,
@@ -81,6 +99,9 @@ class TestNsone(unittest.TestCase):
         self.assertEqual(result, zones)
 
     def test_get_zone_name(self):
+        """
+        Testing get_zone_name method
+        """
         zones = ['example.com', 'test.example.com']
         zone = "test.example.com"
         domain = "_acme-challenge.test.example.com"
@@ -91,6 +112,9 @@ class TestNsone(unittest.TestCase):
 
     @patch("lemur.plugins.lemur_acme.nsone.current_app")
     def test_create_txt_record_write_only(self, mock_current_app):
+        """
+        Testing create_txt_record without any existing data
+        """
         domain = "_acme_challenge.test.example.com"
         zone = "example.com"
         token = "ABCDEFGHIJ"
@@ -116,14 +140,20 @@ class TestNsone(unittest.TestCase):
 
     @patch("lemur.plugins.lemur_acme.nsone.current_app")
     def test_create_txt_record_append(self, mock_current_app):
+        """
+        Testing the create_txt_record with existing answers
+        """
         domain = "_acme_challenge.test.example.com"
         zone = "test.example.com"
         token = "ABCDEFGHIJ"
         account_number = "1234567890"
         change_id = (domain, token)
         nsone._check_conf = Mock()
-        cur_token = "123456"
-        cur_records = {"answers":[{"answer":["ABCDEFHG"],"id":"1234567890abcdef"}],"id":"1234567890abcdef"}
+        cur_records = {
+            "answers":[{
+                "answer":["ABCDEFHG"],"id":"1234567890abcdef"
+            }],
+            "id":"1234567890abcdef"}
         nsone._get_txt_records = Mock(return_value=cur_records)
         nsone._get_zone_name = Mock(return_value=zone)
         mock_current_app.logger.debug = Mock()
@@ -138,7 +168,12 @@ class TestNsone(unittest.TestCase):
             "message": "TXT record(s) successfully created"
         }
         expected_path = "/v1/zones/test.example.com/_acme_challenge.test.example.com/TXT"
-        expected_payload = {"answers": [{"answer": ["ABCDEFHG"], "id": "1234567890abcdef"}, {"answer": ["ABCDEFGHIJ"]}], "id": "1234567890abcdef"}
+        expected_payload = {
+            "answers": [
+                {"answer": ["ABCDEFHG"], "id": "1234567890abcdef"},
+                {"answer": ["ABCDEFGHIJ"]}
+                ],
+            "id": "1234567890abcdef"}
         result = nsone.create_txt_record(domain, token, account_number)
         mock_current_app.logger.debug.assert_called_with(log_data)
         nsone._patch.assert_called_with(expected_path, expected_payload)
@@ -149,6 +184,9 @@ class TestNsone(unittest.TestCase):
     @patch("lemur.extensions.metrics")
     @patch("time.sleep")
     def test_wait_for_dns_change(self, mock_sleep, mock_metrics, mock_current_app, mock_dnsutil):
+        """
+        Testing the wait_for_dns_change method
+        """
         domain = "_acme-challenge.test.example.com"
         token1 = "ABCDEFG"
         token2 = "HIJKLMN"
@@ -177,6 +215,9 @@ class TestNsone(unittest.TestCase):
 
     @patch("lemur.plugins.lemur_acme.nsone.current_app")
     def test_delete_txt_record(self, mock_current_app):
+        """
+        Testing the delete_txt_record method.
+        """
         domain = "_acme-challenge.test.example.com"
         zone = "test.example.com"
         token = "ABCDEFGHIJ"
