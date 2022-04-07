79a0a2f159bbf1643d421222e706f9008bfebdb8
Hossein Shafagh <hshafagh@netflix.com>
2022-04-06 18:08:08 -0700

    DB consistency changes

--- lemur/domains/models.py
+++ lemur/domains/models.py
@@ -7,21 +7,14 @@
 .. moduleauthor:: Kevin Glisson <kglisson@netflix.com>
 
 """
-from sqlalchemy import Column, Integer, String, Boolean, Index
+from sqlalchemy import Column, Integer, String, Boolean
 
 from lemur.database import db
 
 
 class Domain(db.Model):
     __tablename__ = "domains"
-    __table_args__ = (
-        Index(
-            "ix_domains_name_gin",
-            "name",
-            postgresql_ops={"name": "gin_trgm_ops"},
-            postgresql_using="gin",
-        ),
-    )
+    __table_args__ = ()
     id = Column(Integer, primary_key=True)
     name = Column(String(256), index=True)
     sensitive = Column(Boolean, default=False)
