9efaaa595968459c027f1cbc16b879523415ee06
Jared Crawford <jaredcrawford@netflix.com>
2023-06-08 15:01:18 -0500

    Infer type annotations

--- lemur/tests/conftest.py
+++ lemur/tests/conftest.py
@@ -345,3 +345,32 @@ def aws_credentials():
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
+
+# Configuration for pytest to automatically collect types.
+# Thanks to Guilherme Salgado.
+
+import pytest
+
+
+def pytest_collection_finish(session):
+    """Handle the pytest collection finish hook: configure pyannotate.
+
+    Explicitly delay importing `collect_types` until all tests have
+    been collected.  This gives gevent a chance to monkey patch the
+    world before importing pyannotate.
+    """
+    from pyannotate_runtime import collect_types
+    collect_types.init_types_collection()
+
+
+@pytest.fixture(autouse=True)
+def collect_types_fixture():
+    from pyannotate_runtime import collect_types
+    collect_types.start()
+    yield
+    collect_types.stop()
+
+
+def pytest_sessionfinish(session, exitstatus):
+    from pyannotate_runtime import collect_types
+    print(collect_types.dumps_stats())
