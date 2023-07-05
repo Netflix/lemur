70a461c6689ce01abd73785772f64a181121f9a7
Jared Crawford <jaredcrawford@netflix.com>
2023-07-05 11:15:02 -0500

    update test

--- lemur/plugins/lemur_openssl/tests/test_openssl.py
+++ lemur/plugins/lemur_openssl/tests/test_openssl.py
@@ -2,7 +2,7 @@ from unittest import mock
 
 import pytest
 
-from lemur.plugins.lemur_openssl.plugin import run_process
+from lemur.plugins.lemur_openssl.plugin import run_process, get_openssl_version
 from lemur.tests.vectors import INTERNAL_PRIVATE_KEY_A_STR, INTERNAL_CERTIFICATE_A_STR
 
 
@@ -33,4 +33,4 @@ def test_export_certificate_to_pkcs12_legacy(app):
     with mock.patch('lemur.plugins.lemur_openssl.plugin.run_process', mock.Mock(wraps=run_process)) as mock_run_process:
         p.export(INTERNAL_CERTIFICATE_A_STR, "", INTERNAL_PRIVATE_KEY_A_STR, options)
     assert mock_run_process.call_count == 1
-    assert "-legacy" in mock_run_process.call_args_list[0][0][0]
+    assert ("-legacy" in mock_run_process.call_args_list[0][0][0] or get_openssl_version() < b'3')
