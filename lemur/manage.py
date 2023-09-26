34fb0f45c4604f9258b624386747ec105e3b6438
Ebag333 <ebagola@gmail.com>
2023-09-26 23:58:37 +0000

    Linting!

--- lemur/manage.py
+++ lemur/manage.py
@@ -148,7 +148,7 @@ SQLALCHEMY_DATABASE_URI = "postgresql://lemur:lemur@localhost:5432/lemur"
 # VERISIGN_LAST_NAME = ""
 # VERSIGN_EMAIL = ""
 
-# Set of controls to use around ingesting user group information from the IDP 
+# Set of controls to use around ingesting user group information from the IDP
 # Allows mapping user groups to Lemur roles and automatically creating them
 IDP_GROUPS_KEYS = ["googleGroups"]  # a list of keys used by IDP(s) to return user groups (profile[IDP_GROUPS_KEY])
 # Note that prefix/suffix can be commented out or set to "" if no filtering against naming convention is desired
@@ -161,7 +161,7 @@ IDP_ASSIGN_ROLES_FROM_USER_GROUPS = True  # Assigns a Lemur role for each group 
 IDP_CREATE_ROLES_FROM_USER_GROUPS = True  # Creates a Lemur role for each group found attached to the user if missing
 # Protects the built-in groups and prevents dynamically assigning users to them. Prevents IDP admin from becoming
 # Lemur admin. Use IDP_ROLES_MAPPING to create a mapping to assign these groups if desired. eg {"admin": "admin"}
-IDP_PROTECT_BUILTINS = True  
+IDP_PROTECT_BUILTINS = True
 IDP_CREATE_PER_USER_ROLE = True  # Generates Lemur role for each user (allows cert assignment to a single user)
 
 """
