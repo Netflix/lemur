8185af800f6a5bb722a060fac6aacffc7251d8c9
Oleg Dopertchouk <odopertchouk@squarespace.com>
2024-04-09 12:32:19 -0400

    Google CA plugin + its dependencies

--- lemur/plugins/lemur_google_ca/__init__.py
+++ lemur/plugins/lemur_google_ca/__init__.py
@@ -2,4 +2,3 @@ try:
     VERSION = __import__("pkg_resources").get_distribution(__name__).version
 except Exception as e:
     VERSION = "unknown"
-
--- lemur/plugins/lemur_google_ca/plugin.py
+++ lemur/plugins/lemur_google_ca/plugin.py
@@ -1,26 +1,100 @@
+"""
+.. module: lemur.plugins.lemur_google_ca.plugin
+    :platform: Unix
+    :synopsis: This module is responsible for creating certificates with the Google CA API '
+    :license: Apache, see LICENSE for more details.
+
+    Google CA (v2 API) Documentation
+    https://cloud.google.com/certificate-authority-service/docs/reference/rest
+
+    This plugin requires following packages:
+    - google-cloud-private-ca
+    - protobuf
+    - types-protobuf (for mypy)
+    Make sure to add these to `requirements.in`
+
+.. moduleauthor:: Oleg Dopertchouk <odopertchouk@squarespace.com>
+"""
 import json
-import os
+import re
 import uuid
+from typing import Optional
 
 import google.cloud.security.privateca_v1 as private_ca
+
+import arrow
 from flask import current_app
+from google.oauth2 import service_account
 from google.protobuf import duration_pb2
 
-from lemur.exceptions import InvalidConfiguration
-from lemur.plugins import lemur_google_ca as gca, VERSION
+from lemur.constants import CRLReason
+from lemur.common.utils import validate_conf
+import lemur.plugins.lemur_google_ca
 from lemur.plugins.bases import IssuerPlugin
+
+import google.cloud.security.privateca_v1 as privateca_v1
+
+SECONDS_PER_YEAR = 365 * 24 * 60 * 60
+
+
+def get_duration(options):
+    """
+    Deduce certificate duration from options
+    """
+    validity_end = options.get("validity_end")
+    if validity_end:
+        return int((validity_end - arrow.utcnow()).total_seconds())
+    else:
+        return options.get("validity_years", 1) * SECONDS_PER_YEAR
+
+
+def generate_certificate_id(common_name) -> str:
+    """
+    Generates a readable unique id for a cert based on cert's CN
+    """
+    name = common_name.lower().strip()
+    name = re.sub(r'[^a-z0-9-]', '_', name)
+    name = name[:50]  # leave space for random id
+    return f"{name}-{uuid.uuid4().hex}"[:63]  # Truncate to 63 characters, to fit the api constraints
+
+
+def fetch_authority(ca_path: str) -> tuple[str, str]:
+    client = privateca_v1.CertificateAuthorityServiceClient()
+    resp = client.get_certificate_authority(name=ca_path)
+    if resp.state != privateca_v1.CertificateAuthority.State.ENABLED:
+        raise Exception("The CA is not enabled")
+    certs = list(resp.pem_ca_certificates)
+    ca_pem = certs[0]
+    ca_chain = '\n'.join(certs[1:])
+    return ca_pem, ca_chain
 
 
 class GoogleCaIssuerPlugin(IssuerPlugin):
     title = "Google CA"
     slug = "googleca-issuer"
     description = "Enables the creation of certificates by Google CA"
-    version = gca.VERSION
+    version = lemur.plugins.lemur_google_ca.VERSION
 
     author = "Oleg Dopertchouk"
-    author_url = "https://github.com/sqsp"
+    author_url = "https://github.com/odopertchouk"
 
     options = [
+        {
+            "name": "Project",
+            "type": "str",
+            "required": True,
+            "value": "lemur",
+            "validation": "(?i)^[a-zA-Z_0-9.-]+$",
+            "helpMessage": "Must be a valid GCP project name!",
+        },
+        {
+            "name": "Location",
+            "type": "str",
+            "required": True,
+            "value": "us-central1",
+            "validation": "(?i)^[a-z0-9-]+$",
+            "helpMessage": "Must be a valid GCP location name!",
+        },
         {
             "name": "CAPool",
             "type": "str",
@@ -30,11 +104,96 @@ class GoogleCaIssuerPlugin(IssuerPlugin):
             "helpMessage": "Must be a valid GCP name!",
         },
         {
-            "name": "Duration",
-            "type": "int",
-            "required": False,
-            "value": 365,
-            "validation": "(?i)[0-9]+$",
-            "helpMessage": "Duration in days",
+            "name": "CAName",
+            "type": "str",
+            "required": True,
+            "validation": "(?i)^[a-zA-Z_0-9.-]+$",
+            "helpMessage": "Must be a valid GCP name!",
         },
     ]
+
+    def __init__(self, *args, **kwargs):
+        """Initialize source with appropriate details."""
+        required_vars = [
+            "GOOGLE_APPLICATION_CREDENTIALS",
+        ]
+        validate_conf(current_app, required_vars)
+
+    def create_certificate(self, csr, options) -> tuple[str, str, str]:
+        """
+        :param csr: Certificate Signing Request to turn into a certificate
+        :param options: Options passed from the UI (validated by CertificateInputSchema)
+        """
+        authority = options['authority']
+        if not authority:
+            raise ValueError("Certificate  requires a signer CA to be specified")
+        if authority.plugin_name != GoogleCaIssuerPlugin.slug:
+            raise ValueError("Certificate must be created by Google CA")
+        ca_options = {opt['name']: opt['value'] for opt in json.loads(authority.options)}
+        ca_path = f"projects/{ca_options['Project']}" \
+                  f"/locations/{ca_options['Location']}" \
+                  f"/caPools/{ca_options['CAPool']}"
+
+        lifetime = get_duration(options)
+
+        client = private_ca.CertificateAuthorityServiceClient(
+            credentials=service_account.Credentials.from_service_account_file(
+                current_app.config['GOOGLE_APPLICATION_CREDENTIALS']
+            )
+        )
+        request = private_ca.CreateCertificateRequest(
+            parent=ca_path,
+            certificate=private_ca.Certificate(
+                pem_csr=csr,
+                lifetime=duration_pb2.Duration(seconds=lifetime)
+            ),
+            certificate_id=generate_certificate_id(options['common_name']),
+            issuing_certificate_authority_id=ca_options['CAName']
+        )
+        resp = client.create_certificate(request)
+        cert_pem = resp.pem_certificate
+        chain_pem = '\n'.join(resp.pem_certificate_chain)
+        ext_id = request.certificate_id
+        return cert_pem, chain_pem, ext_id
+
+    def create_authority(self, options: dict) -> tuple[str, Optional[str], str, list[dict]]:
+        """
+        :param options: Plugin options as specified in AuthorityInputSchema
+        :return body, private_key, chain, roles
+        """
+        plugin_options = {opt['name']: opt.get('value') for opt in options.get('plugin', {}).get('plugin_options', [])}
+
+        ca_name = options["name"]
+        ca_path = f"projects/{plugin_options['Project']}" \
+                  f"/locations/{plugin_options['Location']}" \
+                  f"/caPools/{plugin_options['CAPool']}" \
+                  f"/certificateAuthorities/{plugin_options['CAName']}"
+        ca_pem, chain_pem = fetch_authority(ca_path)
+
+        name = f"googleca_ca_name_{ca_name}_admin"
+        role = {"username": "", "password": "", "name": name}
+        return ca_pem, "", chain_pem, [role]
+
+    def revoke_certificate(self, certificate, reason):
+        authority = certificate.authority
+        if not authority:
+            raise ValueError("Certificate  requires a signer CA to be specified")
+        if authority.plugin_name != GoogleCaIssuerPlugin.slug:
+            raise ValueError("Certificate must be created by Google CA")
+
+        ca_options = {opt['name']: opt['value'] for opt in json.loads(authority.options)}
+        ca_path = f"projects/{ca_options['Project']}" \
+                  f"/locations/{ca_options['Location']}" \
+                  f"/caPools/{ca_options['CAPool']}" \
+                  f"/certificates/{certificate.external_id}"
+        crl_reason = CRLReason.unspecified
+        if "crl_reason" in reason:
+            crl_reason = CRLReason[reason["crl_reason"]]
+
+        client = private_ca.CertificateAuthorityServiceClient()
+        request = privateca_v1.RevokeCertificateRequest(
+            name=ca_path,
+            reason=crl_reason,
+        )
+        response = client.revoke_certificate(request=request)
+        return response
